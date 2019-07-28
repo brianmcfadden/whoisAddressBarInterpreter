0f81d0cedfdeabe2b50a07ada344b8d253c721b7
Brian McFadden <brianmmcfadden@gmail.com>
2019-07-28 11:39:38 -0400

    ICANN requires captcha, use domaintools instead

--- background.js
+++ background.js
@@ -3,16 +3,19 @@
 // this script converts from
 // https://www.google.com/search?client=ubuntu&channel=fs&q=whois+bob.com&ie=utf-8&oe=utf-8
 // to
-// https://whois.icann.org/en/lookup?name=bob.com
+// http://whois.domaintools.com/bob.com
 
 var pattern="https://www.google.com/search?client=ubuntu&channel=fs&q=whois+*";
+
+// var whoisPrefix = "https://whois.icann.org/en/lookup?name=";
+var whoisPrefix = "http://whois.domaintools.com/";
 
 function redirect(requestDetails) {
   console.log("Loading: " + requestDetails.url);
   var str = requestDetails.url;
   var domain = str.replace("https://www.google.com/search?client=ubuntu&channel=fs&q=whois+", "").replace("&ie=utf-8&oe=utf-8","");
   console.log("Domain: " + domain);
-  var newUrl = "https://whois.icann.org/en/lookup?name=" + domain;
+  var newUrl = whoisPrefix + domain;
   console.log("New URL: " + newUrl);
   return {
     redirectUrl: newUrl
