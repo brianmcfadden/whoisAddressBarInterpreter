5dbb1e529158da5b7c8aae83d1b200de0e20ad80
Brian McFadden <brianmmcfadden@gmail.com>
2019-07-31 02:10:06 -0400

    ping, too

--- whoisAddressBarInterpreter.js
+++ whoisAddressBarInterpreter.js
@@ -4,15 +4,25 @@
 // https://www.google.com/search?client=ubuntu&channel=fs&q=whois+bob.com&ie=utf-8&oe=utf-8
 // to some other helpful information provider, preferably one that doesn't captcha or
 // limit requests
+//
+// and it does pings, now, too..
 
 var pattern="https://www.google.com/search?client=*&q=whois+*";
+var anotherPattern="https://www.google.com/search?client=*&q=ping+*";
 
 var whoisPrefix = "https://mxtoolbox.com/SuperTool.aspx?action=whois%3a";
+var pingPrefix = "https://www.ultratools.com/tools/pingResult?hostName=";
 
 function redirect(requestDetails) {
   var str = requestDetails.url;
-  var domain = str.replace("https://www.google.com/search?client=ubuntu&channel=fs&q=whois+", "").replace("&ie=utf-8&oe=utf-8","");
-  var newUrl = whoisPrefix + domain;
+  var newUrl;
+  if (str.match(/whois/g)) {
+    var domain = str.replace(/https:\/\/www.google.com\/search\?client=.*&q=whois\+/, "").replace(/&ie=.*$/,"");
+    newUrl = whoisPrefix + domain;
+  } else if (str.match(/ping/g)) {
+    var address = str.replace(/https:\/\/www.google.com\/search\?client=.*&q=ping\+/, "").replace(/&ie=.*$/,"");
+    newUrl = pingPrefix + address;
+  }
   return {
     redirectUrl: newUrl
   };
@@ -20,6 +30,6 @@ function redirect(requestDetails) {
 
 browser.webRequest.onBeforeRequest.addListener(
   redirect,
-  {urls: [pattern], types: ["main_frame"]},
+  {urls: [pattern, anotherPattern], types: ["main_frame"]},
   ["blocking"]
 );
